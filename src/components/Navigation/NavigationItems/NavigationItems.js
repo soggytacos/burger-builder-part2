dff5898371325c9dbf73525a9665a727ab771ca3
Colman <colman.swisher@gmail.com>
2020-05-22 22:40:49 -0500

    render the orders option in the toolbar only when authenticated.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,10 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
+        {props.isAuthenticated
+            ? <NavigationItem link="/orders">Orders</NavigationItem>
+            : null
+        }
         {props.isAuthenticated
             ? <NavigationItem link="/logout">Sign Out</NavigationItem>
             : <NavigationItem link="/auth">Sign In</NavigationItem>
