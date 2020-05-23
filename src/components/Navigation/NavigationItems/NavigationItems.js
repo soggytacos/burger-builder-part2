f7a64a36e926025b2a6177b85be007aed504885d
Colman <colman.swisher@gmail.com>
2020-05-22 22:25:07 -0500

    changed the state of the navigation bar depending on the users authentication status

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,14 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Sign In</NavigationItem>
+        {props.isAuthenticated
+            ? <NavigationItem link="/logout">Sign Out</NavigationItem>
+            : <NavigationItem link="/auth">Sign In</NavigationItem>
+        }
     </ul>
 );
 
